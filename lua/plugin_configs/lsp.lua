033c3de5c795265b24d77b7e00d9ff53ec44b665
root <root@s31821>
2026-04-07 13:08:51 +0800

    lsp update

--- lua/plugin_configs/lsp.lua
+++ lua/plugin_configs/lsp.lua
@@ -17,54 +17,82 @@ if mason_status_ok then
 	})
 end
 
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	return
+end
+
 local mason_lspconfig_status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
 if mason_lspconfig_status_ok then
+	-- =====================================================================
+	-- 在 setup 内部使用 handlers
+	-- =====================================================================
 	mason_lspconfig.setup({
 		ensure_installed = {
 			"vtsls",
+			"clangd",
 			"cssls",
 			"html",
-			"clangd",
 		},
 		automatic_installation = true,
-	})
-end
 
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if lspconfig_status_ok then
-	local servers = { "vtsls", "cssls", "html", "volar" }
-	for _, server in ipairs(servers) do
-		local has_server, _ = pcall(function()
-			return lspconfig[server]
-		end)
-		if has_server and lspconfig[server] then
-			pcall(function()
-				lspconfig[server].setup({
+		-- 核心魔法：这就是替代 setup_handlers 的最新写法！
+		handlers = {
+			-- 1. 默认处理函数：自动接管所有未特殊指定的 LSP
+			function(server_name)
+				lspconfig[server_name].setup({
 					capabilities = capabilities,
 				})
-			end)
-		end
-	end
+			end,
 
-	local has_clangd, _ = pcall(function()
-		return lspconfig.clangd
-	end)
-	if has_clangd and lspconfig.clangd then
-		pcall(function()
-			lspconfig.clangd.setup({
-				capabilities = vim.tbl_deep_extend("force", capabilities, {
-					offsetEncoding = { "utf-8", "utf-16" },
-					textDocument = {
-						completion = {
-							editsNearCursor = true,
+			-- 2. 特殊处理区
+
+			["vtsls"] = function()
+				local mason_registry = require("mason-registry")
+				local vue_language_server_path = ""
+				pcall(function()
+					local vue_pkg = mason_registry.get_package("vue-language-server")
+					vue_language_server_path = vue_pkg:get_install_path() .. "/node_modules/@vue/language-server"
+				end)
+
+				lspconfig.vtsls.setup({
+					capabilities = capabilities,
+					filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+					settings = {
+						vtsls = {
+							tsserver = {
+								globalPlugins = {
+									{
+										name = "@vue/typescript-plugin",
+										location = vue_language_server_path,
+										languages = { "vue" },
+									},
+								},
+							},
 						},
 					},
-				}),
-				cmd = { "clangd" },
-				filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-				root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
-				single_file_support = true,
-			})
-		end)
-	end
+				})
+			end,
+
+			["volar"] = function()
+				lspconfig.volar.setup({
+					capabilities = capabilities,
+					init_options = { vue = { hybridMode = true } },
+				})
+			end,
+
+			["clangd"] = function()
+				lspconfig.clangd.setup({
+					capabilities = vim.tbl_deep_extend("force", capabilities, {
+						offsetEncoding = { "utf-8", "utf-16" },
+						textDocument = { completion = { editsNearCursor = true } },
+					}),
+					cmd = { "clangd" },
+					filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+					root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
+					single_file_support = true,
+				})
+			end,
+		},
+	})
 end
--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,66 +1,73 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-      require("plugin_configs.lsp")
-    end
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("plugin_configs.lsp")
+		end,
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-      "hrsh7th/cmp-path",
-    },
-    config = function()
-      require("plugin_configs.cmp")
-    end
-  },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			require("plugin_configs.cmp")
+		end,
+	},
 
-  {
-    "numToStr/Comment.nvim",
-    config = function()
-      require("plugin_configs.comment")
-    end
-  },
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("plugin_configs.comment")
+		end,
+	},
 
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("plugin_configs.autopairs")
-    end
-  },
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("plugin_configs.autopairs")
+		end,
+	},
 
-  -- ==========================================
-  -- 🌟 新增的专业格式化工具 Conform
-  -- ==========================================
-  {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("conform").setup({
-        format_on_save = {
-          timeout_ms = 500,
-          -- 这里的 lsp_fallback = true 完美解决了你的顾虑：
-          -- 如果找不到专门的格式化工具，它会自动回退使用 LSP 进行格式化！
-          lsp_fallback = true,
-        },
-        formatters_by_ft = {
-          lua = { "stylua" },
-          javascript = { "prettier" },
-          typescript = { "prettier" },
-          cpp = { "clang-format" },
-          c = { "clang-format" },
-        },
-      })
-    end,
-  },
+	-- ==========================================
+	-- 🌟 新增的专业格式化工具 Conform
+	-- ==========================================
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("conform").setup({
+				format_on_save = {
+					timeout_ms = 500,
+					-- 这里的 lsp_fallback = true 完美解决了你的顾虑：
+					-- 如果找不到专门的格式化工具，它会自动回退使用 LSP 进行格式化！
+					lsp_fallback = true,
+				},
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettierd" },
+					typescript = { "prettierd" },
+					javascriptreact = { "prettierd" },
+					typescriptreact = { "prettierd" },
+					vue = { "prettierd" },
+					css = { "prettierd" },
+					html = { "prettierd" },
+					json = { "prettierd" },
+					markdown = { "prettierd" },
+					cpp = { "clang-format" },
+					c = { "clang-format" },
+				},
+			})
+		end,
+	},
 }
